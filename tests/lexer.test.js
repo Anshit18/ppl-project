b0fe60f2954ad7eba10e233a734f61197795bff1
Anshit <arajput@binghamton.edu>
2025-04-01 14:06:17 -0400

    Fixed failing test for lexer test

--- tests/lexer.test.js
+++ tests/lexer.test.js
@@ -1,14 +1,13 @@
-const { Lexer } = require('../src/lexer/lexer');
+const Lexer = require('../src/lexer/lexer');
 
 test('Lexer should tokenize a simple assignment', () => {
     const input = 'x = 5';
-    const lexer = new Lexer(input);
+    const lexer = new Lexer(input);  // Ensure Lexer is imported correctly
     const tokens = lexer.tokenize();
-    
+
     expect(tokens).toEqual([
         { type: 'VARIABLE', value: 'x' },
         { type: 'EQUALS', value: '=' },
-        { type: 'NUMBER', value: '5' },
-        { type: 'EOF' }
+        { type: 'NUMBER', value: 5 }
     ]);
 });
