02ded509dd4eb8ff89d0519609e52537276ff819
Anshit <arajput@binghamton.edu>
2025-04-01 12:17:22 -0400

    pushing lexer tests

---
+++ tests/lexer.test.js
@@ -0,0 +1,14 @@
+const { Lexer } = require('../src/lexer/lexer');
+
+test('Lexer should tokenize a simple assignment', () => {
+    const input = 'x = 5';
+    const lexer = new Lexer(input);
+    const tokens = lexer.tokenize();
+    
+    expect(tokens).toEqual([
+        { type: 'VARIABLE', value: 'x' },
+        { type: 'EQUALS', value: '=' },
+        { type: 'NUMBER', value: '5' },
+        { type: 'EOF' }
+    ]);
+});
